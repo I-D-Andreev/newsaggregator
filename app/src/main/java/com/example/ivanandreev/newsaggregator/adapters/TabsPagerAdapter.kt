bb1cbb845be76eb9115351142c8c87f3e161ead7
Ivan Andreev <iivvaann.andreev@gmail.com>
2020-10-27 19:10:11 +0000

    Fix deprecated constructor

--- app/src/main/java/com/example/ivanandreev/newsaggregator/adapters/TabsPagerAdapter.kt
+++ app/src/main/java/com/example/ivanandreev/newsaggregator/adapters/TabsPagerAdapter.kt
@@ -2,13 +2,12 @@ package com.example.ivanandreev.newsaggregator.adapters
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
 import androidx.fragment.app.FragmentStatePagerAdapter
 import com.example.ivanandreev.newsaggregator.fragments.AccountKeywordsFragment
 import com.example.ivanandreev.newsaggregator.fragments.AccountTopicsFragment
 
 class TabsPagerAdapter(fm: FragmentManager, private val tabTitles: Array<String>) :
-    FragmentStatePagerAdapter(fm) {
+    FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
     override fun getCount(): Int {
         return tabTitles.size
     }
