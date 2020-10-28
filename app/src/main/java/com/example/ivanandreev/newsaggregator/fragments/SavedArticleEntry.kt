d69038f95ab10a5220503c4960aff058fd2976dc
Ivan Andreev <iivvaann.andreev@gmail.com>
2020-10-28 19:07:34 +0000

    SavedArticleEntry

--- app/src/main/java/com/example/ivanandreev/newsaggregator/fragments/SavedArticleEntry.kt
+++ app/src/main/java/com/example/ivanandreev/newsaggregator/fragments/SavedArticleEntry.kt
@@ -1,2 +1,18 @@
 package com.example.ivanandreev.newsaggregator.fragments
 
+class SavedArticleEntry {
+    var title: String = ""
+    var publisher: String = ""
+    var image : Int = 0
+//    var removeButton: MaterialButton? = null
+//    summary
+//    http link
+
+    constructor(){}
+    constructor(title_: String, publisher_ : String, image_: Int){
+        this.title = title_
+        this.publisher = publisher_
+        this.image = image_
+    }
+
+}
