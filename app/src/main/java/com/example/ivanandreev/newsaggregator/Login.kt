b9128538cebfcc94d2a5cfff5925521df54ab8a8
Ivan Andreev <iivvaann.andreev@gmail.com>
2020-11-10 17:20:41 +0000

    Password input and password "eye" button

--- app/src/main/java/com/example/ivanandreev/newsaggregator/Login.kt
+++ app/src/main/java/com/example/ivanandreev/newsaggregator/Login.kt
@@ -1,11 +1,27 @@
 package com.example.ivanandreev.newsaggregator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+
 
 class Login : AppCompatActivity() {
+    private var mAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_screen)
+        mAuth = FirebaseAuth.getInstance()
+
+    }
+
+    private fun signIn(){
+        val email_textbox = findViewById<TextInputEditText>(R.id.email)
+        val password_textbox = findViewById<TextInputEditText>(R.id.password)
+
+        val email = email_textbox.text
+        val password = password_textbox.text
+//        mAuth.createUserWithEmailAndPassword(email, )
     }
 }
