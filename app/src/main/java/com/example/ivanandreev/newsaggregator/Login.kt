e632e1d4858b37869480b9f15d3c5564f668406b
Ivan Andreev <iivvaann.andreev@gmail.com>
2020-11-20 03:23:16 +0000

    Remove unnecessary import

--- app/src/main/java/com/example/ivanandreev/newsaggregator/Login.kt
+++ app/src/main/java/com/example/ivanandreev/newsaggregator/Login.kt
@@ -8,7 +8,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 
 // user: t@t.com
 // password: 123456
