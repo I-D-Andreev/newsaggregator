77bf65238cabb2cc6e214fbe2e0bcac48af403a6
Ivan Andreev <iivvaann.andreev@gmail.com>
2020-11-21 06:31:53 +0000

    News article position on click

--- app/src/main/java/com/example/ivanandreev/newsaggregator/adapters/NewsArticleAdapter.kt
+++ app/src/main/java/com/example/ivanandreev/newsaggregator/adapters/NewsArticleAdapter.kt
@@ -19,38 +19,40 @@ import java.util.*
 class NewsArticleAdapter(private val newsArticlesList: MutableList<NewsEntry>) :
     RecyclerView.Adapter<NewsArticleAdapter.ViewHolder>() {
 
-    inner class ViewHolder(var layout: View) : RecyclerView.ViewHolder(layout)
+    inner class ViewHolder(val layout: View) : RecyclerView.ViewHolder(layout) {
+        init {
+            layout.setOnClickListener(this::onItemClicked)
+        }
+
+        private fun onItemClicked(view: View){
+            val ctx: Context = view.context
+            val actions: Array<String> = ctx.resources.getStringArray(R.array.news_article_actions)
+            val articlePosition: Int = this.layoutPosition
+
+            val dialog: AlertDialog = AlertDialog.Builder(view.context)
+                .setTitle(ctx.getString(R.string.choose_action))
+                .setItems(actions) {_: DialogInterface?, which: Int ->
+                    when(which){
+                        0 -> visitArticle(articlePosition)
+                        1 -> saveArticle(articlePosition)
+                    }
+                }
+                .setNegativeButton(ctx.getString(R.string.cancel), null)
+                .create()
+            dialog.show()
+        }
+    }
+
+    private fun saveArticle(position: Int) {
+    }
+
+    private fun visitArticle(position: Int){
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val view = inflater.inflate(R.layout.news_article_entry, parent, false)
-        view.setOnClickListener(this::onItemClicked)
         return ViewHolder(view)
-    }
-
-    private fun onItemClicked(view: View){
-        val ctx: Context = view.context
-        val actions: Array<String> = ctx.resources.getStringArray(R.array.news_article_actions)
-
-        val dialog: AlertDialog = AlertDialog.Builder(view.context)
-            .setTitle(ctx.getString(R.string.choose_action))
-            .setItems(actions) {_: DialogInterface?, which: Int ->
-                when(which){
-                    0 -> visitArticle()
-                    1 -> saveArticle()
-                }
-            }
-            .setNegativeButton(ctx.getString(R.string.cancel), null)
-            .create()
-        dialog.show()
-    }
-
-    private fun visitArticle(){
-
-    }
-
-    private fun saveArticle() {
-
     }
 
     override fun getItemCount(): Int {
@@ -65,7 +67,6 @@ class NewsArticleAdapter(private val newsArticlesList: MutableList<NewsEntry>) :
             .placeholder(R.drawable.white)
             .error(R.drawable.error)
             .load(article.imageUrl)
-
         holder.layout.article_title.text = article.title
         holder.layout.article_date.text = sdf.format(article.date.time)
         holder.layout.article_publisher.text = article.publisher
