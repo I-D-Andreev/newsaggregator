b69617adadb40c2c4c4bafb726c173ab25e98ae2
Ivan Andreev <iivvaann.andreev@gmail.com>
2020-11-21 03:26:35 +0000

    Basic RegisterActivity

--- app/src/main/java/com/example/ivanandreev/newsaggregator/RegisterActivity.kt
+++ app/src/main/java/com/example/ivanandreev/newsaggregator/RegisterActivity.kt
@@ -1,2 +1,18 @@
 package com.example.ivanandreev.newsaggregator
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_screen)
+    }
+
+    fun createAccount(view: View){
+        println("!!! Hello world!")
+    }
+
+}
