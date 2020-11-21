45e62585228ad8c79ecf25926c32df3d58a271d8
Ivan Andreev <iivvaann.andreev@gmail.com>
2020-11-21 04:03:33 +0000

    User registration

--- app/src/main/java/com/example/ivanandreev/newsaggregator/RegisterActivity.kt
+++ app/src/main/java/com/example/ivanandreev/newsaggregator/RegisterActivity.kt
@@ -1,18 +1,59 @@
 package com.example.ivanandreev.newsaggregator
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.ivanandreev.newsaggregator.helpers.Keyboard
+import com.google.android.gms.tasks.Task
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 
 
 class RegisterActivity : AppCompatActivity() {
+    private val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_screen)
     }
 
-    fun createAccount(view: View){
-        println("!!! Hello world!")
+    fun createAccount(view: View) {
+        Keyboard.hideKeyboard(this, view)
+
+        val emailTextBox = findViewById<TextInputEditText>(R.id.email)
+        val passwordTextBox = findViewById<TextInputEditText>(R.id.password)
+        val repeatPasswordTextBox = findViewById<TextInputEditText>(R.id.repeat_password)
+
+        val email = emailTextBox.text.toString()
+        val password = passwordTextBox.text.toString()
+        val repeatPassword = repeatPasswordTextBox.text.toString()
+
+        if (email.isNotEmpty() && password.isNotEmpty() && repeatPassword.isNotEmpty()) {
+            if (password == repeatPassword) {
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {task: Task<AuthResult> ->
+                    if(task.isSuccessful){
+                        println("!!! User creation is successful!")
+                        signInUser()
+                    } else{
+                        Toast.makeText(this, getString(R.string.authentication_failed), Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+            } else {
+                Toast.makeText(this, getString(R.string.passwords_must_match), Toast.LENGTH_SHORT)
+                    .show()
+            }
+        } else {
+            Toast.makeText(this, getString(R.string.all_fields_filled), Toast.LENGTH_SHORT).show()
+        }
     }
 
+    private fun signInUser(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
