2f47cea9771ec46714644090e133187ce66ff577
Ivan Andreev <iivvaann.andreev@gmail.com>
2020-11-29 06:49:12 +0000

    Basic ReadArticleActivity

---
+++ app/src/main/java/com/example/ivanandreev/newsaggregator/ReadArticleActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ivanandreev.newsaggregator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ReadArticleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.read_article_screen)
+    }
+
+}
