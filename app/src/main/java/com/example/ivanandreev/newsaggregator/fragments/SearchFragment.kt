336e9a82cf7dd9f14acadeccf46409d3373fc20c
Ivan Andreev <iivvaann.andreev@gmail.com>
2020-11-29 13:56:42 +0000

    Basic search box

--- app/src/main/java/com/example/ivanandreev/newsaggregator/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/ivanandreev/newsaggregator/fragments/SearchFragment.kt
@@ -2,9 +2,12 @@ package com.example.ivanandreev.newsaggregator.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.fragment.app.Fragment
 import com.example.ivanandreev.newsaggregator.R
+import com.example.ivanandreev.newsaggregator.helpers.Keyboard
 
 
 class SearchFragment : Fragment() {
@@ -19,4 +22,13 @@ class SearchFragment : Fragment() {
         false
     )!!
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val searchButton = view.findViewById<AppCompatImageButton>(R.id.search_button)
+        searchButton.setOnClickListener(this::onSearchButtonClicked)
+    }
+
+    private fun onSearchButtonClicked(view: View){
+        Keyboard.hideKeyboard(view)
+    }
 }
